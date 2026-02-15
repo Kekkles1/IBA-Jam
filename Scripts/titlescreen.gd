d1e38f8f2e94f0c9c799a4e48ce746bc97c1138d
Kekkles1 <fatima.mah24314@gmail.com>
2026-02-15 13:25:15 +0500

    final fr

--- Scripts/titlescreen.gd
+++ Scripts/titlescreen.gd
@@ -1,4 +1,10 @@
 extends Control
 
+@onready var bgm: AudioStreamPlayer = $BGM
+
+func _ready() -> void:
+	bgm.volume_db = -15
+	bgm.play()
+	
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/StartScreen.tscn")
